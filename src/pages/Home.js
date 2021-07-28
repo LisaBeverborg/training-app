5ba8dba47ff7c2f982e91c55f87c4f029b53905b
lisa.beverborg <lisa.beverborg@gmail.com>
2021-07-28 16:38:30 +0200

    daily sports quote api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,27 @@
-function HomePage() {
-    return <div>main page</div>;
-}
-
-export default HomePage;
+import React, { useState, useEffect} from "react";
+import Header from '../components/Header';
+ 
+const HomePage = () => {
+    let [quote, setQuote]= useState(null)
+    let [author, setAuthor]= useState(null)
+ 
+ 
+    useEffect(() => {
+        async function getDailyQuote() {
+            let response = await fetch("https://quotes.rest/qod/sports");
+            const data = await response.json();            
+            setQuote(data.contents.quotes[0].quote);
+            setAuthor(data.contents.quotes[0].author);
+        }
+        getDailyQuote()
+    }, [])
+ 
+ 
+    return <div>
+        <Header></Header>
+        <h2>{quote}</h2>     
+        <h4>{author}</h4>
+    </div>;
+  }
+  
+  export default HomePage;
