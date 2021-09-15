e0ac3d6e4a0b55c3c33eedc7f62db0605a574bd5
lisa.beverborg <lisa.beverborg@gmail.com>
2021-09-15 22:38:00 +0200

    add Footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+
+function Footer () {
+
+  return (
+    <section >
+      <section >
+          <section>
+              Let's train AB
+          </section>       
+        <section>
+          <section>
+            letstrain.info@gmail.com
+          </section>
+          <section>
+            Terms and Conditions
+            <br />
+            Copyright
+          </section>
+        </section>     
+          <section >
+            Contact Us
+            <br />
+            FAQ
+        </section>
+      </section>
+    </section>
+  )
+
+}
+
+export default Footer;
