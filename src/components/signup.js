301b10d69df7066ff369d8291411c875633a3c50
lisa.beverborg <lisa.beverborg@gmail.com>
2021-09-22 21:13:16 +0200

    signup form functionality

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,19 +1,43 @@
-import React, { useRef } from "react"
-import { Link } from "react-router-dom"
+import React, { useRef, useState } from "react"
+import { Link,useHistory } from "react-router-dom"
+import { useAuth } from "../contexts/AuthContext"
 
 export default function Signup() {
     const emailRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
+    const {signup} = useAuth()
+    const [error, setError] = useState("")
+    const [loading, setLoading] = useState(false)
+    const history = useHistory()
 
+    async function handleSubmit(e) {
+        e.preventDefault()
     
+        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+          return setError("Passwords do not match")
+        }
+  
+        try {
+            setError("")
+            setLoading(true)
+            await signup(emailRef.current.value, passwordRef.current.value)
+            history.push("/")
+          } catch {
+            setError("Failed to create an account")
+          }
+      
+          setLoading(false)
+        }
+      
     
-    return (
+  return (
     <>
       <div>
         <div>
           <h2>Sign Up</h2>
-          <form>
+          {error && <div>{error}</div>}
+          <form  onSubmit={handleSubmit}>
             <label id="email">
               <div >Email</div>
               <input type="email" ref={emailRef} required />
@@ -26,7 +50,7 @@ export default function Signup() {
               <div>Password Confirmation</div>
               <input type="password" ref={passwordConfirmRef}required />
             </label>
-            <button type="submit">
+            <button  disabled={loading} type="submit">
               Sign Up
             </button>
           </form>
