72dcfc4edebf9101fdbeacfc679af1900e0bfd71
lisa.beverborg <lisa.beverborg@gmail.com>
2021-10-11 17:49:28 +0200

    footer update

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,19 +1,20 @@
 import React from "react";
+import classes  from "./Footer.module.css";
   
 const Footer = () => {
   return (
-    <div>
+    <div className= {classes.box}>
       <h1>
         Let's train
       </h1>
-      <div>
-          <div>
+      <div className= {classes.container}>
+          <div className= {classes.column}>
             <h2>About Us</h2>
           </div>
-          <div>
+          <div className= {classes.column}>
             <h2>Contact Us</h2>
           </div>
-          <div>
+          <div  className= {classes.column}>
             <h2>FAQ</h2>
         </div>
         </div>
